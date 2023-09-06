b5baf5c00a9ca30141ef32b84543043367731158
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-09-06 18:09:51 +0100

    start project and made product model

---
+++ lib/models/Product.dart
@@ -0,0 +1,14 @@
+// ignore: file_names
+class Product {
+  final String? title, image;
+
+  Product({this.title, this.image});
+}
+
+List<Product> demo_products = [
+  Product(title: 'Product-1', image: 'assets/images/product5.jpg'),
+  Product(title: 'Product-2', image: 'assets/images/product6.jpg'),
+  Product(title: 'Product-3', image: 'assets/images/product7.jpg'),
+  Product(title: 'Product-4', image: 'assets/images/product8.jpg'),
+  Product(title: 'Product-5', image: 'assets/images/product9.jpg'),
+];
